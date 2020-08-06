56c46173a01c9c960a7a93fd9059dfe2dba42143
Jonathan Hutson <jonathanh30@googlemail.com>
2020-08-06 18:25:48 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
 //Welcome page
-router.get('/', (req, res) => res.render('welcome'));
+router.get('/welcome', (req, res) => res.render('welcome'));
 
 //Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) => 
@@ -11,6 +11,12 @@ res.render('dashboard',{
 }));
 
 router.get('/users/update', ensureAuthenticated, (req, res) =>
+res.render('update',{
+    name: req.user.name,
+    email: req.user.email,
+    password: req.user.password
+}));
+router.get('/quiz/quiz', ensureAuthenticated, (req, res) =>
 res.render('update',{
     name: req.user.name,
     email: req.user.email,
